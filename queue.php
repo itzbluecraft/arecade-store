2d935b81edda2b1a9df7ebf2824d765a1e619e75
cj <bluecraft887@gmail.com>
2026-08-11 09:57:58 +0700

    Delete queue.php

--- queue.php
+++
@@ -1,4 +0,0 @@
-<?php
-require __DIR__.'/_common.php';key(true);$a=[];
-foreach(glob($c['data_dir'].'/ARC-*.json')?:[] as $f){$o=json_decode(file_get_contents($f),true);if(($o['status']??'')==='PAID_PENDING_COMMAND')$a[]=$o;}
-out(['ok'=>true,'count'=>count($a),'orders'=>$a]);
